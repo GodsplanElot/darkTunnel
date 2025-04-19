bcc4e5c38db4a0eaf408b5ea4333a06049863156
unknown <elotgodsplan2@gmail.com>
2025-04-19 08:14:21 +0100

    Database Setup (Create Case ID model)

--- main/models.py
+++ main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Case(models.Model):
+    case_id = models.CharField(max_length=12, unique=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
